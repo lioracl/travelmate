05f5302cb3025ed997494b5665af212b6aa71d58
lioracl <lioracl@gmail.com>
2026-09-21 14:26:55 +0300

    Refine card separation and weather blur v144

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v143';
-const ASSET_VERSION='20260921-4';
+const CACHE_NAME='travelmate-smart-v144';
+const ASSET_VERSION='20260921-5';
 const CORE_PATHS=[
   './',
   './index.html',
