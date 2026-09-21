311ba9d1ccd129f33cd24acdbb332b2965ced15e
lioracl <lioracl@gmail.com>
2026-09-21 22:08:45 +0300

    Prepare v1.54.18 Places performance hardening

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v154';
-const ASSET_VERSION='20260921-15';
+const CACHE_NAME='travelmate-smart-v155';
+const ASSET_VERSION='20260921-16';
 const CORE_PATHS=[
   './',
   './index.html',
