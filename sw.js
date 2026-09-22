3587a78bd95fd42e42c8a8aa82edca9789503162
lioracl <lioracl@gmail.com>
2026-09-22 14:51:36 +0300

    Refresh clients after accent picker
    Bump asset and service-worker versions after verified accent picker UI.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v168';
-const ASSET_VERSION='20260922-08';
+const CACHE_NAME='travelmate-smart-v169';
+const ASSET_VERSION='20260922-09';
 const CORE_PATHS=[
   './',
   './index.html',
