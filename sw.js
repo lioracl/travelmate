33d1483f86188fec212053bddb0b0b13521d2ca6
lioracl <lioracl@gmail.com>
2026-09-22 06:56:23 +0300

    Bump assets for place image fix

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v160';
-const ASSET_VERSION='20260921-21';
+const CACHE_NAME='travelmate-smart-v161';
+const ASSET_VERSION='20260922-01';
 const CORE_PATHS=[
   './',
   './index.html',
