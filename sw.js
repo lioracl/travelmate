8c41180f7592016be3cd425150b635bebd9f843f
lioracl <lioracl@gmail.com>
2026-09-22 11:58:25 +0300

    Bump Places design assets for fresh clients

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v166';
-const ASSET_VERSION='20260922-06';
+const CACHE_NAME='travelmate-smart-v167';
+const ASSET_VERSION='20260922-07';
 const CORE_PATHS=[
   './',
   './index.html',
