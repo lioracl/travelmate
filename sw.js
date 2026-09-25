6aef01288b2190e151241b6b9a8eb8cbfa967976
lioracl <lioracl@gmail.com>
2026-09-26 00:34:54 +0300

    Bump service worker to v215

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME='travelmate-smart-v214';
-const ASSET_VERSION='20260925-17';
+const CACHE_NAME='travelmate-smart-v215';
+const ASSET_VERSION='20260925-18';
 const CORE_PATHS=[
   './',
   './index.html',
