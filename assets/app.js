aa19cb1a8ac34198a958164eb8808a35f4760c4d
lioracl <lioracl@gmail.com>
2026-09-26 01:03:15 +0300

    Bump assets to 20260925-21

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,6 @@
 var appScript=document.currentScript;
 (function(){
-  var version=(function(){try{return new URL(appScript.src,location.href).searchParams.get('v')||'20260925-20'}catch(error){return'20260925-20'}})();
+  var version=(function(){try{return new URL(appScript.src,location.href).searchParams.get('v')||'20260925-21'}catch(error){return'20260925-21'}})();
   var loadedStyles={},loadedScripts={};
   var baseStyles=['language.css','mobile-menu.css','navigation-memory.css','network-usage.css','trip-redesign.css','modal-system.css','theme.css'];
   var finalStyle='readable-glass.css';
